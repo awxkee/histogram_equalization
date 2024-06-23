625d7838f4cea8c508fc0455a53f42551004d51c
awxkee <radzivon.bartoshyk@proton.me>
2024-06-23 19:23:25 +0100

    update libs

--- src/app/src/main.rs
+++ src/app/src/main.rs
@@ -2,7 +2,7 @@ use colorutils_rs::rgb_to_rgba;
 use image::GenericImageView;
 use image::io::Reader as ImageReader;
 
-use histogram_equalization::{clahe_hsl_rgba, clahe_hsv_bgra, clahe_hsv_rgba, clahe_lab_rgb, clahe_lab_rgba, clahe_luv_bgra, clahe_luv_rgba, ClaheGridSize, hist_equal_luv_rgb};
+use histogram_equalization::{clahe_hsl_bgra, clahe_hsl_rgba, clahe_hsv_bgra, clahe_hsv_rgba, clahe_lab_rgb, clahe_lab_rgba, clahe_luv_bgra, clahe_luv_rgba, ClaheGridSize, hist_equal_luv_rgb};
 
 fn main() {
     let img = ImageReader::open("assets/IMG_5902.jpg")
@@ -23,13 +23,23 @@ fn main() {
         dimensions.1,
         255,
     );
+
+    let stride = dimensions.0 * 4;
+    for y in 0..dimensions.1 {
+        for x in 0..dimensions.0 {
+            let b = dst_rgba[(y * stride + x * 4) as usize];
+            dst_rgba[(y * stride + x * 4) as usize] = dst_rgba[(y * stride + x * 4 + 2) as usize];
+            dst_rgba[(y * stride + x * 4 + 2) as usize] = b;
+        }
+    }
+
     src_bytes = &dst_rgba;
 
     let channels = 4;
     let stride = dimensions.0 as usize * channels;
     let mut dst_bytes: Vec<u8> = vec![0; stride * dimensions.1 as usize];
 
-    clahe_hsv_bgra(
+    clahe_hsl_bgra(
         src_bytes,
         stride as u32,
         &mut dst_bytes,
@@ -40,6 +50,15 @@ fn main() {
         ClaheGridSize::new(8, 8),
         128,
     );
+
+    let stride = dimensions.0 * 4;
+    for y in 0..dimensions.1 {
+        for x in 0..dimensions.0 {
+            let b = dst_bytes[(y * stride + x * 4) as usize];
+            dst_bytes[(y * stride + x * 4) as usize] = dst_bytes[(y * stride + x * 4 + 2) as usize];
+            dst_bytes[(y * stride + x * 4 + 2) as usize] = b;
+        }
+    }
 
     if channels == 4 {
         image::save_buffer(
