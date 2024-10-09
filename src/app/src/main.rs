84b8ae1bf4524374f458e5b2ccea431b4c96b188
awxkee <radzivon.bartoshyk@proton.me>
2024-10-10 00:09:24 +0100

    Up version, rayon

--- src/app/src/main.rs
+++ src/app/src/main.rs
@@ -5,7 +5,7 @@ use std::time::Instant;
 use histogram_equalization::{ahe_hsl_rgb, ahe_hsv_rgb, ahe_lab_rgb, ahe_yuv_rgb, clahe_jzazbz_bgra, clahe_jzazbz_rgb, clahe_lab_bgra, clahe_lab_rgb, clahe_luv_bgra, clahe_luv_rgb, clahe_oklab_bgra, clahe_oklab_rgb, clahe_oklab_rgba, clahe_oklch_bgra, clahe_yuv_bgra, clahe_yuv_rgb, hist_equal_hsl_rgb, hist_equal_lab_rgb, hist_equal_luv_rgb, ClaheGridSize};
 
 fn main() {
-    let img = ImageReader::open("assets/asset_1.jpg")
+    let img = ImageReader::open("assets/forest.jpg")
         .unwrap()
         .decode()
         .unwrap();
@@ -20,13 +20,14 @@ fn main() {
 
     let start_time = Instant::now();
 
-    ahe_hsv_rgb(
+    clahe_oklab_rgb(
         src_bytes,
         stride as u32,
         &mut dst_bytes,
         stride as u32,
         dimensions.0,
         dimensions.1,
+        1.3f32,
         ClaheGridSize::new(8, 8),
         128,
     );
